e3d4280b85eca8b81a94f9b0509db704bd425525
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-16 13:07:52 +0700

    variabel dart

---
+++ dart_project/bin/2_Variable/var2.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  var umur = 17;
+  dynamic nama = 'Sidra Febrian Hardiyanto';
+  String kota = 'Jepara';
+
+print('Nama saya $nama, saya berumur $umur tahun, saya tinggal di $kota');
+}
+
