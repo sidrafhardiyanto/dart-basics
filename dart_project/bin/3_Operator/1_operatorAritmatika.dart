a45c8a5d8f986b8322092501c52c916644cd2b14
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-16 13:14:16 +0700

    tambah operator aritmatika

---
+++ dart_project/bin/3_Operator/1_operatorAritmatika.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  int c = 6;
+  c = c + 6; // c = 12
+
+  print('c = $c');
+
+  c = c - 10; // c = 2
+  print('c = $c');
+
+  c = c * 2; // c = 4
+  print('c = $c');
+
+  double d = 10;
+  d = d / 3; // c = 3.33
+  print('d = $d');
+
+  c = c % 2; // c = 0
+  print('c = $c');
+}
