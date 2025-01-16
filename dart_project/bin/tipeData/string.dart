17fbab9d3d3bee789b89a73f2ca28f1b09b89f4c
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-16 11:42:50 +0700

    tambah tipe data string

---
+++ dart_project/bin/tipeData/string.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  // String adalah kumpulan huruf, angka, dan tanda baca lainnya. Jadi isinya bebas asalkan tulisan.
+  String data = "Sidra Febrian Hardiyanto - 221240001231";
+  print(data);
+}
