aa206b6be2c6d3044e7e3f04dbf8829c458350c7
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-16 14:48:05 +0700

    materi loop

---
+++ dart_project/bin/7_Looping/1_loopFor.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  int i;
+  for(i = 1; i <= 10; i++) { 
+    print("Number $i");
+  }
+}
---
+++ dart_project/bin/7_Looping/3_loopDoWhile.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  int i=1;
+  do {
+    print("nilai while: $i");
+    i++;
+  } while(i<=10);
+}
