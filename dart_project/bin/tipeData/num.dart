cd96a851934471dbdadc894bc012da6f511f0d88
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-16 11:45:55 +0700

    tambah tipe data num

---
+++ dart_project/bin/tipeData/num.dart
@@ -0,0 +1,7 @@
+void main() {
+  // Num, merupakan bilangan bulat dan bilangan pecahan. Tipe data ini bisa menjadi integer ataupun double, sesuai nanti data yang diinputkan.
+  num data = 19;
+  print(data);
+  num data2 = 1902.2;
+  print(data2);
+}
