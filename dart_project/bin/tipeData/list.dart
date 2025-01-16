8d6cecd3be5a8a5cdd64ccf5828432fa9ae9d5bd
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-16 11:52:38 +0700

    tambah tipe data list

---
+++ dart_project/bin/tipeData/list.dart
@@ -0,0 +1,6 @@
+void main() {
+  // List merupakan tumpukan data. Jika kalian familiar dengan java atau javascript, List merupakan sebuah Array.
+
+  List data = [1,2,3,'a','b', 'c'];
+  print(data);
+}
