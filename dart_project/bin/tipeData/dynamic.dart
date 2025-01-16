4911bd63e217af7c0fc780ad7c12b5de5fc8a6eb
Sidra Febrian Hardiyanto <sidrafhardiyanto@gmail.com>
2025-01-16 13:06:56 +0700

    tambah tipe data dynamic

---
+++ dart_project/bin/tipeData/dynamic.dart
@@ -0,0 +1,6 @@
+// Dynamic merupakan tipe data yang isinya bisa berubah, dynamic bisa berupa integer, string, double, num, object, dan lain sebagainya.
+void main() {
+    dynamic a = 12.2;
+    a = 'Hello Sidra Febrian Hardiyanto!';
+    print(a);
+}
